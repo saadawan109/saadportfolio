e5b13164300145821ed3449d09e9c39513a379ce
Muhammad Awais <167178601+saadawan109@users.noreply.github.com>
2024-09-12 04:44:52 -0700

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,44 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
+  return (
+    <>
+      <section className="footer_section">
+        <footer className="container py-4">
+          <div className="row py-2">
+            <div className="col-md-5">
+              <h5 className="footer_heading">Contact</h5>
+              <address className="text-white">
+                Lodhran, Punjab, Pakistan
+                <br />
+                Email:{" "}
+                <a
+                  href="mailto:your.email@example.com"
+                  className="text-secondary text-decoration-none"
+                >
+                  maliksaad109109@gmail.com
+                </a>
+              </address>
+            </div>
 
-        <>
-            <section className='footer_section'>
-                <footer className="container py-4">
-                    <div className="row py-2 d-flex text-align-center">
-                        <div className='col-md-4'>
-                            <h5 className='footer_heading'>Contact</h5>
-                            <address className='text-white'>
-                                Lodhran, Punjab, Pakistan
-                                <br />
-                                Email:{" "}
-                                <a href="mailto:your.email@example.com" className='text-secondary text-decoration-none'>maliksaad109109@gmail.com</a>
-                            </address>
-                        </div>
-                            
-                        <div className='col-md-2'>
-                        <h5 className='footer_heading'>Contact</h5>
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="https://github.com/saadawan109"  className='text-secondary text-decoration-none'>GitHub</a>
-                                </li>
-                                <li>
-                                    <a href="https://vercel.com/muhammad-saads-projects-7920679b"  className='text-secondary text-decoration-none'>Li</a>
-                                </li>
-                            </ul>
-                        </div>        
-                    </div>
-                    <div className="row py-2 d-flex text-align-center">
-                        <p className='text-white'>@ 2024 Muhammad Saad Tarique All right are reserved</p>
-                    </div>
-                </footer>
-            </section>
+            <div className="col-md-2">
+              <h5 className="footer_heading">Contact</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a
+                    href="https://github.com/saadawan109"
+                    className="text-secondary text-decoration-none"
+                  >
+                    GitHub
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/malik-saad-awan?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                    className="text-secondary text-decoration-none"
+                  >
+                    LinkdIn
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row py-2 pt-2 ">
+            <div className="col-md-5">
+              <p className="text-white">
+                @ 2024 Muhammad Saad Tarique All right are reserved
+              </p>
+            </div>
+          </div>
+        </footer>
+      </section>
+    </>
+  );
+};
 
-
-
-        </>
-    )
-}
-
-export default Footer
+export default Footer;
